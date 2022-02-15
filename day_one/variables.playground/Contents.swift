5cc7a7049680f0917b900b979b71eab0bb02acb1
Mustafa TÜRKMEN <iletisim@mustafatrkmn.com>
2022-02-15 14:09:32 +0300

    day one complete

---
+++ day_one/variables.playground/Contents.swift
@@ -0,0 +1,51 @@
+import UIKit
+
+
+// String Test
+var str = "Hello, World!!!"
+
+str = "Good bye"
+
+// Int test
+var age = 8
+
+var population = 8_000_000
+
+// Multiline test
+var str1 = """
+hello
+world
+how are you?
+I'm tring to multi line string
+"""
+
+
+var str2 = """
+another \
+multi \
+line \
+test
+"""
+
+// Double Test
+var pi = 3.141
+
+// Bool Test
+var isAwesome = true
+
+// String Interpolation Test
+var scores = 100
+
+var stringScores = "your scores is \(scores)"
+
+// Constant Test
+let constTry = "An unchangable string"
+
+// Type Annotation Test
+let name: String = "Mustafa"
+
+let personAge: Int = 24
+
+let hasDriveLicence: Bool = true
+
+let height: Double = 174.9
